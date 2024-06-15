b1dc3c75aab4c33e4b22331b29d4d56119aab9bc
arman-007 <armanhosen9@gmail.com>
2024-06-16 00:46:52 +0600

    base templated & total visit counter functionalities added

--- src/core/views.py
+++ src/core/views.py
@@ -1,6 +1,20 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from visits.models import PageVisits
 
 
 def home_page_view(request, *args, **kwargs):
-
-    return HttpResponse("<h1> Hello World! </h1>")
+    qs = PageVisits.objects.all()
+    page_visit_qs = PageVisits.objects.filter(path=request.path)
+    context = {
+        "page_title" : "HomePage",
+        "page_visit_count" : page_visit_qs.count(),
+        "percent_page_visit" : (page_visit_qs.count() * 100) / qs.count(),
+        "total_visit_count" : qs.count(),
+    }
+    html_template = "home.html"
+    PageVisits.objects.create(
+        path=request.path
+    )
+    return render(request, html_template, context)
