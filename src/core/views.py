a5e3ef1b99c7a9031baefdcedbe9c62f6ed1062e
arman-007 <armanhosen9@gmail.com>
2024-06-16 16:00:46 +0600

    prepare for railway

--- src/core/views.py
+++ src/core/views.py
@@ -7,10 +7,33 @@ from visits.models import PageVisits
 def home_page_view(request, *args, **kwargs):
     qs = PageVisits.objects.all()
     page_visit_qs = PageVisits.objects.filter(path=request.path)
+    try:
+        percent  = (page_visit_qs.count() * 100) / qs.count()
+    except:
+        percent = 0
     context = {
         "page_title" : "HomePage",
         "page_visit_count" : page_visit_qs.count(),
-        "percent_page_visit" : (page_visit_qs.count() * 100) / qs.count(),
+        "percent_page_visit" : percent,
+        "total_visit_count" : qs.count(),
+    }
+    html_template = "home.html"
+    PageVisits.objects.create(
+        path=request.path
+    )
+    return render(request, html_template, context)
+
+def about_view(request, *args, **kwargs):
+    qs = PageVisits.objects.all()
+    page_visit_qs = PageVisits.objects.filter(path=request.path)
+    try:
+        percent  = (page_visit_qs.count() * 100) / qs.count()
+    except:
+        percent = 0
+    context = {
+        "page_title" : "About US",
+        "page_visit_count" : page_visit_qs.count(),
+        "percent_page_visit" : percent,
         "total_visit_count" : qs.count(),
     }
     html_template = "home.html"
