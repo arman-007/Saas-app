202f94441754a7a723b512fded89693bb9614afe
arman-007 <armanhosen9@gmail.com>
2024-06-18 04:16:20 +0600

    template updated & vendor pull cmmand added

---
+++ src/commando/management/commands/hello_world.py
@@ -0,0 +1,7 @@
+from typing import Any
+from django.core.management import BaseCommand
+
+class Command(BaseCommand):
+    
+    def handle(self, *args: Any, **options: Any):
+        print("Hello World!")
--- src/core/settings.py
+++ src/core/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
     #installed apps
     "visits",
+    "commando",
 ]
 
 MIDDLEWARE = [
@@ -140,6 +141,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_BASE_DIR = BASE_DIR / "staticfiles"
+STATICFILES_BASE_DIR.mkdir(parents=True, exist_ok=True)
+STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR / "vendors"
+
+#source(s) for python manage.py collectstatic
+STATICFILES_DIRS = [
+    STATICFILES_BASE_DIR
+]
+
+#output for python manage.py collectstatic
+STATIC_ROOT = BASE_DIR / "local-cdn"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
